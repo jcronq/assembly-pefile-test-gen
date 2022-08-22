a8fe0f7a96bad1733f5695eb3745ed0ced4fddaa
Jason Cronquist <jcronq@gmail.com>
2022-08-22 15:50:43 -0400

    initial commit

---
+++ test/compile.bat
@@ -0,0 +1,4 @@
+set projectName=simple-mov
+cd %projectName%
+\masm32\bin\ml /c /Zd /coff .\test.asm 
+\masm32\bin\Link /SUBSYSTEM:CONSOLE .\test.obj
---
+++ test/simple-mov/test.asm
@@ -0,0 +1,16 @@
+.386
+; test assmebly
+.model flat, stdcall
+option casemap :none
+
+include \masm32\include\kernel32.inc
+include \masm32\include\masm32.inc
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\masm32.lib
+.data
+
+.code
+
+main:
+    mov ecx,10h
+end main
